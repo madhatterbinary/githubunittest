1fe5a29d8aab43e5c0ce3bfd55d63207a77ca817
Ulysses ALvarez <ulyssesbytes@gmail.com>
2018-05-08 17:51:40 +0100

    First commit

---
+++ calc.js
@@ -0,0 +1,15 @@
+const add = (a, b) => {
+    return a + b;
+}
+
+const substract = (a, b) => {
+    return "";
+}
+
+const multiply = (a, b) => {
+    return "";
+}
+
+const divide = (a, b) => {
+    return "";
+}
